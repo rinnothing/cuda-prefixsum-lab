ddbe84aa8ec71d949e8db814b7e3b18db85b3d61
rinnothing <58170199+rinnothing@users.noreply.github.com>
2026-05-21 17:55:20 +0300

    implement host code except arguments parsing

---
+++ gen.py
@@ -0,0 +1,33 @@
+from random import random
+import struct
+
+def binary_uint32_t(num):
+    return struct.pack("I", num)
+
+def gen_arr(x):
+    arr = []
+    for i in range(x):
+        arr.append(int(random() * 10))
+
+    return arr
+
+
+def write_arr(f, arr):
+    for i in range(len(arr)):
+        f.write(binary_uint32_t(arr[i]))
+            # f.write(f'{arr[j][i]:.2f} ')
+        # f.write('\n')
+
+def gen(name, x):
+    a = gen_arr(x)
+
+    f = open(name, "wb")
+    # f.write(f'{x} {m} {y}\n')
+    f.write(binary_uint32_t(x))
+
+    write_arr(f, a)
+
+if __name__ == "__main__":
+    x = 10
+
+    gen("input.txt", x)
--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 #include <cstdio>
 #include <vector>
 
+#include <omp.h>
+
 struct cmdArgs {
     int deviceIndex;
     char* input;
     char* output;
     bool verify;
+
+    unsigned int cpuBlockSize;
 };
 
 int parseArgs(int argc, char* argv[], cmdArgs* args) {
@@ -15,6 +19,13 @@ int parseArgs(int argc, char* argv[], cmdArgs* args) {
     args->input = "input.txt";
     args->output = "output.txt";
     args->verify = false;
+
+    args->cpuBlockSize = 64;
+
+    
+    // debug
+    args->verify = true;
+    args->cpuBlockSize = 16;
 
     //todo
     return 0;
@@ -25,7 +36,34 @@ struct calcTask {
 };
 
 int readTask(cmdArgs* args, calcTask* task) {
-    // todo
+    std::FILE* f = std::fopen(args->input, "rb");
+    if (f == NULL) {
+        std::fprintf(stderr, "can't open input file");
+        return 1;
+    }
+
+    unsigned int n;
+    if (std::fread(&n, sizeof(unsigned int), 1, f) != 1) {
+        std::fprintf(stderr, "can't parse task length");
+        std::fclose(f);
+        return 1;
+    }
+
+    task->arr.resize(n);
+    if (std::fread(&(*task->arr.begin()), sizeof(unsigned int), n, f) != n) {
+        std::fprintf(stderr, "can't parse task contents");
+        std::fclose(f);
+        return 1;
+    }
+
+    
+    //debug
+    std::printf("task values are:");
+    for (int i = 0; i < n; i++) {
+        std::printf(" %i", task->arr[i]);
+    }
+    std::printf("\n");
+
     return 0;
 }
 
@@ -37,7 +75,25 @@ struct calcRes {
 };
 
 int writeRes(cmdArgs* args, calcRes* res) {
-    // todo
+    std::FILE* f = std::fopen(args->output, "wb");
+    if (f == NULL) {
+        std::fprintf(stderr, "can't open output file");
+        return 1;
+    }
+
+    unsigned int n = res->arr.size();
+    if (std::fwrite(&n, sizeof(unsigned int), 1, f) != 1) {
+        std::fprintf(stderr, "can't write res length");
+        std::fclose(f);
+        return 1;
+    }
+
+    if (std::fwrite(&(*res->arr.begin()), sizeof(unsigned int), n, f) != n) {
+        std::fprintf(stderr, "can't write res contents");
+        std::fclose(f);
+        return 1;
+    }
+
     return 0;
 }
 
@@ -46,13 +102,58 @@ int calcCUDA(cmdArgs* args, calcTask* task, calcRes* res) {
     return 0;
 }
 
-int calcCPU(calcTask* task, calcRes* res) {
-    // todo
+int calcCPU(cmdArgs* args, calcTask* task, calcRes* res) {
+    unsigned int n = task->arr.size();
+    res->arr.resize(n);
+
+    #pragma omp parallel for
+    for (unsigned int b = 0; b < n; b += args->cpuBlockSize) {
+        unsigned int brd = std::min(args->cpuBlockSize, n-b);
+        res->arr[b] = task->arr[b];
+        for (unsigned int i = 1; i < brd; i++) {
+            res->arr[b+i] = res->arr[b+i-1] + task->arr[b+i];
+        }
+    }
+
+    unsigned int ttl = 0;
+    for (unsigned int i = args->cpuBlockSize-1; i < n; i += args->cpuBlockSize) {
+        res->arr[i] += ttl;
+        ttl = res->arr[i];
+    }
+
+    #pragma omp parallel for
+    for (unsigned int b = args->cpuBlockSize; b < n; b += args->cpuBlockSize) {
+        unsigned int brd = std::min(args->cpuBlockSize - 1, n-b);
+        unsigned int pref = res->arr[b-1];
+        for (unsigned int i = 0; i < brd; i++) {
+            res->arr[b+i] += pref;
+        }
+    }
+
+    //debug
+    std::printf("cpu values are:");
+    for (int i = 0; i < n; i++) {
+        std::printf(" %i", res->arr[i]);
+    }
+    std::printf("\n");
+    
     return 0;
 }
 
-int compareResults(calcRes* cudaRes, calcRes* cpuRes) {
-    // todo
+int compareResults(cmdArgs* args, calcRes* cudaRes, calcRes* cpuRes) {
+    if (cudaRes->arr.size() != cpuRes->arr.size()) {
+        std::fprintf(stderr, "results length aren't equal cpu - %zi, cuda - %zi", cpuRes->arr.size(), cudaRes->arr.size());
+        return 1;
+    }
+    
+    int n = cudaRes->arr.size();
+    for (int i = 0; i < n; i++) {
+        if (cudaRes->arr[i] != cpuRes->arr[i]) {
+            std::fprintf(stderr, "results are different in %i symbol - cpu %i, cuda %i", i, cpuRes->arr[i], cudaRes->arr[i]);
+            return 1;
+        }
+    }
+
     return 0;
 }
 
@@ -66,7 +167,7 @@ int main(int argc, char* argv[]) {
     int devCount;
     cudaError_t cudaErr = cudaGetDeviceCount(&devCount);
     if (cudaErr) {
-        fprintf(stderr, "can't get cuda device number: error %i", cudaErr);
+        std::fprintf(stderr, "can't get cuda device number: error %i", cudaErr);
         return 1;
     }
 
@@ -75,17 +176,17 @@ int main(int argc, char* argv[]) {
     cudaDeviceProp devProp;
     cudaErr = cudaGetDeviceProperties(&devProp, args.deviceIndex);
     if (cudaErr) {
-        fprintf(stderr, "can't get cuda device properties: error %i", cudaErr);
+        std::fprintf(stderr, "can't get cuda device properties: error %i", cudaErr);
         return 1;
     }
 
     int driverVersion;
     cudaErr = cudaDriverGetVersion(&driverVersion);
     if (cudaErr) {
-        fprintf(stderr, "can't get cuda driver version: error %i", cudaErr);
+        std::fprintf(stderr, "can't get cuda driver version: error %i", cudaErr);
     }
 
-    printf("Device: %s\tDriver version: %i\n", devProp.name, driverVersion);
+    std::printf("Device: %s\tDriver version: %i\n", devProp.name, driverVersion);
 
     calcTask task;
     err = readTask(&args, &task);
@@ -99,7 +200,7 @@ int main(int argc, char* argv[]) {
         return err;
     }
 
-    printf("Time: %g\t%g\n", cudaRes.kernelTime, cudaRes.fullTime);
+    std::printf("Time: %g\t%g\n", cudaRes.kernelTime, cudaRes.fullTime);
 
     err = writeRes(&args, &cudaRes);
     if (err) {
@@ -108,14 +209,14 @@ int main(int argc, char* argv[]) {
 
     if (args.verify) {
         calcRes cpuRes;
-        err = calcCPU(&task, &cpuRes);
+        err = calcCPU(&args, &task, &cpuRes);
         if (err) {
             return err;
         }
 
-        printf("Time CPU: %g\n", cpuRes.fullTime);
+        std::printf("Time CPU: %g\n", cpuRes.fullTime);
 
-        err = compareResults(&cudaRes, &cpuRes);
+        err = compareResults(&args, &cudaRes, &cpuRes);
         if (err) {
             return err;
         }
